a21fc295e7dbee583a01841af2d0e78e5078fbc2
luanmarcal <luan.miguel13@hotmail.com>
2024-04-26 22:36:55 -0300

    📝 add exercise_19

---
+++ review-exercises/Exercise19.java
@@ -0,0 +1,14 @@
+// Exercício 19
+// Elaborar um programa que escreva em tela os números de 20 a 1, utilizando o comando for(
+// ).
+
+
+public class Exercise19 {
+
+    public static void main(String[] args) {
+        for (int i = 20; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+    
+}
