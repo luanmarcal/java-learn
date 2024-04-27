a28a3b9814bcdf837617bc072923672424ad6070
luanmarcal <luan.miguel13@hotmail.com>
2024-04-26 22:39:55 -0300

    📝 add exercise_22

---
+++ review-exercises/Exercise22.java
@@ -0,0 +1,15 @@
+// Exercício 22
+// Elaborar um programa que escreva em tela os números de 1 a 20, utilizando o comando do-
+// while( ).
+
+
+public class Exercise22 {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= 20);
+    }
+}
