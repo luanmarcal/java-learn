b9ed8cb7fe958e6a2ee641577ee444d7368538ff
luanmarcal <luan.miguel13@hotmail.com>
2024-04-26 22:54:55 -0300

    📝 add exercise_37

---
+++ review-exercises/Exercise37.java
@@ -0,0 +1,30 @@
+// Exercício 37
+// Ordene um array em ordem descendente.
+
+import java.util.Arrays;
+
+public class Exercise37 {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 3, 82, 5, 6, 7, 8, 9};
+
+        System.out.println("\nOrdem original:");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = temp;
+        }
+
+        System.out.println("\n\nOrdem decrescente:");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        System.out.println("\n");
+    }
+}
