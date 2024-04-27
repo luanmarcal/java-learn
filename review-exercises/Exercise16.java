89b0ea609d54ed93946f1d1a2545777ea058af93
luanmarcal <luan.miguel13@hotmail.com>
2024-04-26 22:33:55 -0300

    📝 add exercise_16

---
+++ review-exercises/Exercise16.java
@@ -0,0 +1,16 @@
+// Exercício 16
+// Elaborar um programa que escreva em tela os números de 1 a 20, utilizando o comando
+// while( ).
+
+
+public class Exercise16 {
+    public static void main(String[] args) {
+
+        int num = 1;
+
+        while (num <= 20) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
